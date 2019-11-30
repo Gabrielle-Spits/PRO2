0fed653a60d2f9c2b3344c877e9e2bf5d90480e9
Gabrielle Spits <gabrielle-spits@hotmail.com>
2019-11-30 22:02:12 +0100

    nieuwe balk

--- navigation.php
+++ navigation.php
@@ -1,29 +1,30 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#"></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="./index.php?content=tekstrow3">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./index.php?content=verlanglijstje">verlanglijstje</a>
-              </li>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">opleidingen</a>
-                  <a class="dropdown-item" href="#">uitstromingen</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="./index.php?content=tekstrow3">Home <span class="sr-only">(current)</span></a>
+      </li>
+
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          opleidingen
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">politie</a>
+          <a class="dropdown-item" href="#">bedrijven</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php?content=verlanglijstje">aanmelden</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php?content=verlanglijstje">game</a>
+      </li>
+    </ul>
+  </div>
+</nav>
