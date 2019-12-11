903f18b392ee7fcecece76bcbabc55f0cf369c96
Gabrielle Spits <gabrielle-spits@hotmail.com>
2019-12-11 06:11:56 +0100

    lekker bezig

--- footer.php
+++ footer.php
@@ -1,18 +1,28 @@
 <div container>
     <div class="row">
         <div class="col-6">
-            <h2> adres:</h2>
-            <br>
-            Daltonlaan 300
-            <br>
-            3584 BK Utrecht
-            <br>
-            Nederland
+            <h4> adres:</h4>
+            <h5>
+                Daltonlaan 300
+            </h5>
+            <h5>
+                3584 BK Utrecht
+            </h5>
+            <h5>
+                Nederland
+            </h5>
         </div>
         <div class="col-6">
-
-
+            <h4>contact</h4>
+            <h5>
+                WhatsApp: 06-25705051 (op schooldagen bereikbaar tussen 10.00 en 17.00 uur)
+            </h5>
+            <h5>
+                Mail: studiekeuze@mboutrecht.nl
+            </h5>
+            <h5>
+                Telefoon: 030 – 281 51 00 (op schooldagen bereikbaar tussen 08.30 en 16.30 uur)
+            </h5>
         </div>
     </div>
-
 </div>
