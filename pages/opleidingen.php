24859c4e10b3e28d6eac8c48513091088f72b4b0
Gabrielle Spits <gabrielle-spits@hotmail.com>
2019-12-24 12:18:14 +0100

    no message

--- pages/opleidingen.php
+++ pages/opleidingen.php
@@ -20,7 +20,8 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-6">
+    <div class="col-2"></div>
+    <div class="col-5">
         <div class="card" style="width: 20rem;">
             <img src="./img/politie.jpg" class="card-img-top" alt="...">
             <div class="card-body">
@@ -30,16 +31,16 @@
             </div>
         </div>
     </div>
-    <div class="col-6">
-        <div class="card" style="width: 18rem;">
-            <img src="./img/bedrijven.PNG" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title"></h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="./index.php?content=bedrijven" class="btn btn-primary">Go somewhere</a>
+        <div class="col-4">
+             <div class="card" style="width: 18rem;">
+                 <img src="./img/bedrijven.PNG" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="./index.php?content=bedrijven" class="btn btn-primary">Go somewhere</a>
+                    </div>
             </div>
         </div>
-    </div>
 </div>
 
 
